877fec74f68d2d1f57abcc274e7f6a82082114d4
yamashita1001 <kd1358584@st.kobedenshi.ac.jp>
2024-05-09 13:58:25 +0900

    0509データ送信

---
+++ public/get.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GETメソッドのデータ</title>
+</head>
+
+<body>
+    <h4>0J01035山下雄生</h4>
+    <p>GETメソッドで送られてきたパラメータ名dataの値は、
+        <?php
+        echo $_GET['data'] . " です。</p>";
+        ?>
+</body>
+
+</html>
